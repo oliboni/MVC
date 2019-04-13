d601af1fc4caed6db72f8b61cfc3179b1512aeab
Rodrigo Oliboni <rodrigo_oliboni@hotmail.com>
2019-04-13 13:02:08 -0300

    Criado model Author

---
+++ ExemploControllers/app/Controllers/AuthorsControllers.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = express.Router()
+const models = require('../models')
+const bodyParser = require('body-parser')
+router.use(bodyParser.urlencoded({extended: true}))
+router.use(bodyParser.json())
+
+//Create
+router.post('/', function (req, res) {
+    models.Author.create(req.body).then(
+        author => res.status(200).send(author)
+    ).catch(err => res.status(500).send(err))
+})
+
+//Get all
+router.get('/', function(req, res) {
+    models.Author.findAll().then(
+        authors => res.status(200).send(authors)
+    )
+})
+
+//Find one by id
+router.get('/:id', function(req, res) {
+    models.Author.findByPk(req.params.id)
+        .then(author => {
+            if (!author) {
+                res.status(404).send("NOT FOUND")
+            }
+
+            res.status(200).send(author)
+        }).catch(err => res.status(500).send(err))
+})
+
+//Update
+router.put('/:id', function(req, res) {
+    models.Author.findByPk(req.params.id).then(author => {
+        if (!author) {
+            res.status(404).send("NOT FOUND")
+        }
+
+        author.update(req.body)
+        res.status(200).send(author)
+    })
+
+})
+
+//Delete
+router.delete('/:id', function(req, res){
+    models.Author.destroy({
+        where: {id: req.params.id}
+    }).then(author => {
+        res.status(200).send('Excluído com sucesso')
+    })
+})
+
+module.exports = router
---
+++ ExemploControllers/index.js
@@ -0,0 +1,23 @@
+//node_modules/.bin/sequelize model:genegare
+// --nome Author --attributes first_name:string.last_name:string é como criarmos alguns arquivos como models migrations
+var express = require('express')
+var app = express()
+var AuthorsControllers = require("./app/Controllers/AuthorsControllers")
+
+/*app.get('/', function(req,res) {
+    res.send('Hello World!')
+})
+
+app.get('/teste', function(req, res){
+    res.status(200).send('Página Teste')
+})
+
+app.listen(3000, function() {
+    console.log("Servidor ouvindo na porta 3000!")
+})*/
+
+app.use("/authors", AuthorsControllers)
+
+app.listen(3000, function(){
+    console.log("Server ouvindo na porta 3000!")
+})
